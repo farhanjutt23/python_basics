1ea77dbe4409cf1e7833f3d2dbe91665d5abd0ce
M Farhan ur Rasool <farhanshafiq2023@gmail.com>
2023-11-07 04:30:39 +0500

    there we can change

---
+++ area.py
@@ -0,0 +1,5 @@
+#two find the area of the triangle 
+base=45
+height=56.8
+area=(1/2)*height*base
+print("the area of the given triangle =" , area)
---
+++ practice.py
@@ -0,0 +1,7 @@
+# write a program that enter the ur name in the diff varible and then print the their name your can b provide by the google]
+first="Muhammad"
+second="Farhan"
+third="ur "
+fourth="Rasool"
+name=first+second+third+fourth
+print(name)
